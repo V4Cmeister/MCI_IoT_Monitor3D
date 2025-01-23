f11d405115cad012deaf2e7f4c8c67bb0e726cf5
V4Cmeister <nikolaussoukopf@gmail.com>
2025-01-23 14:57:31 +0100

    added MQTT connection

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import streamlit as st
 
+""" import paho.mqtt.client as mqtt
+from credentials import MQTT_USERNAME, MQTT_PASSWORD, MQTT_BROKER, MQTT_PORT
+
+# Define the callback function for when a message is received
+def on_message(client, userdata, message):
+    print(f"Received message: {message.payload.decode()} on topic {message.topic}")
+
+# Define the callback function for when the client connects to the broker
+def on_connect(client, userdata, flags, rc, properties=None):
+    if rc == 0:
+        print("Connected successfully")
+        # Subscribe to a topic
+        client.subscribe("Filament_Supervision/Microcontroller_Last_Seen")
+    else:
+        print(f"Connection failed with code {rc}")
+
+# Create an MQTT client instance
+client = mqtt.Client()
+
+# Assign the callback functions
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to the MQTT broker
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+
+# Start the loop to process received messages
+client.loop_forever() """
+
 def main():
     # Set Streamlit app to centered layout
     st.set_page_config(page_title="Uni Login", layout="centered")
